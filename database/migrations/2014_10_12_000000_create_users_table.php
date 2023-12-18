f5bd1905ab908de2127fff0b2c3bb686daaddb8f
Mohamed Hussein <105607296+MohamedHussein43@users.noreply.github.com>
2023-12-18 12:49:20 +0200

    resolve create user table migration conflicts

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -7,22 +7,29 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
-     */
+     * Run the mig     */
     public function up(): void
     {
-        Schema::table('users', function ( $table) {
-            
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
-            
+            $table->timestamps();
         });
     }
 
     /**
+
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
